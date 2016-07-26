878100f93bb38a6c1ce15a0ec59560fac3a2527e
Justin Clark-Casey <justincc@justincc.org>
2016-07-26 18:13:36 +0100

    load our biolta file as n-triples for now as rdflib isn't liking nquads for some reason (loads but nothing is in IOMemory store)

--- src/pygiron.py
+++ src/pygiron.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
 
+import jargparse
 import rdflib
 
-g = rdflib.Graph()
-g.load('http://dbpedia.org/resource/Semantic_Web')
+parser = jargparse.ArgParser('Graph database in Python')
+parser.add_argument('path', help='path to an initial input N-Quads file')
+args = parser.parse_args()
 
+g = rdflib.Graph()
+g.load(args.path, format='nt')
+
+print len(g)
+print len(g)
 for s, p, o in g:
     print s, p, o
